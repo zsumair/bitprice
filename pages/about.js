0a9a9cdf1fc6b9c5f74f0399aa80bdde4d2ecf10
syedzoheb1242@gmail.com <syedzoheb1242@gmail.com>
2019-10-19 00:22:27 +0530

    Simple Bitcoin price app in NextJS

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const about = () => (
+  <Layout>
+    <div>
+      <h1>About Bitprice</h1>
+      <p>Simple about page</p>
+    </div>
+  </Layout>
+);
+
+export default about;
